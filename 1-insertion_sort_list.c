2135a6506a3a9afbf32b860ddd3d9184e8c43cc4
xilder <olabowaleayobami@gmail.com>
2023-09-23 22:38:21 +0000

    updates 1-insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * of integers using the insertion method
+ *
+ * @list: the unsorted list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *set, *curr, *prev;
+
+	set = *list;
+	if (set == NULL || set->next == NULL)
+	       return;	
+
+	set = set->next;
+	while (set != NULL)
+	{	
+		curr = set;
+		while (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			prev = curr->prev;
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			prev->next = curr->next;
+			curr->next = prev;
+			curr->prev = prev->prev;
+			prev->prev = curr;
+			if (curr->prev != NULL)
+				curr->prev->next = curr;
+			else
+				*list = curr;
+			print_list((*list));
+		}
+		set = set->next;
+	}
+}
