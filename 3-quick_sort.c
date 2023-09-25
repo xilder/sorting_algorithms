e36d6508b358281785dab973a90fe24f880f335b
xilder <olabowaleayobami@gmail.com>
2023-09-25 17:54:24 +0000

    adds 3--quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+/**
+ * swap - Function that swaps two values
+ * 
+ * @a: First value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+
+
+void quick(int *array, int left, int right, size_t size);
+/**
+ * quick - the sorting algorithm
+ *
+ * @array: array to be sorted
+ * @left: left of the array
+ * @right: right of the array
+ * @size: size of the array
+ */
+void quick(int *array, int left, int right, size_t size)
+{
+	int pivot;
+	int i = left - 1, j;
+	if (left >= right)
+		return;
+
+	pivot = array[right];
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i < j)
+			{
+				swap(array + j, array + i);
+				print_array(array, size);
+			}
+		}
+	}
+	swap(array + i + 1, array + j);
+	print_array(array, size);
+	quick(array, left, i, size);
+	quick(array, i + 2, right, size);
+}
+
+/**
+ * quick_sort - sorts an array of data using quicksort algorithms
+ *
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick(array, 0, size - 1, size);
+}
+
+
+
--- sort.h
+++ sort.h
@@ -21,4 +21,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
 #endif /* _SORT_H_ */
